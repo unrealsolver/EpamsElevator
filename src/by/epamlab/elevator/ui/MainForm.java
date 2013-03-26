92d7624325a8184e99bd202a85c796095ff8c23b
Ruslan Panasiuk <solver@tut.by>
2013-03-26 17:28:30 +0300

    Basic event handling
    * Proigral

--- src/by/epamlab/elevator/ui/MainForm.java
+++ src/by/epamlab/elevator/ui/MainForm.java
@@ -3,6 +3,8 @@ package by.epamlab.elevator.ui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
@@ -11,7 +13,7 @@ public class MainForm extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	
 	public MainForm() {
 		super();
 		initUI();
@@ -19,7 +21,7 @@ public class MainForm extends JFrame {
 	
 	public final void initUI() {
 		setTitle("Elevator GUI");
-		setSize(300, 200);
+		//setSize(400, 200);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		//setResizable(false);
@@ -27,19 +29,36 @@ public class MainForm extends JFrame {
 		JPanel canvas = new JPanel();
 		canvas.setBackground(Color.gray);
 		canvas.setPreferredSize(new Dimension(260, 380));
-		canvas.setMinimumSize(new Dimension(260, 380)); //WUT?
+		canvas.setMinimumSize(new Dimension(260, 380)); //WUT? //Do not work, srsly.
 		
-		JTextArea text = new JTextArea("Hello texty!");
+		final JTextArea text = new JTextArea("Вы еще не проигрывали сегодня?\n");
 		text.setEditable(false);
 		
-		JButton godButton = new JButton("<Do something>");
+		final JButton godButton = new JButton("Проиграть");
+		final class Cell {
+			private int value; 
+			public Cell(int t) { value = t; }
+			public void set(int t) { value = t; }
+			public int get() { return value; }
+			public void inc() { value += 1; };
+			public String toString() { return String.valueOf(value); };
+		}
+		final Cell proigralRaz = new Cell(0);
+		godButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				proigralRaz.inc();
+				text.append("Сколько раз вы проигрывали: " + proigralRaz + "\n"); //Do not know how to workaround \n
+				//TODO: ELEVATION_TASK -> GET_STATE -> DETERMINE -> SWITCH STATE AND CAPTION
+				godButton.setText("Проиграно: " + proigralRaz);
+			}
+		});
 		
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
 		buttonPanel.add(godButton);
 		
 		JPanel mainPanel = new JPanel(new BorderLayout());
-		mainPanel.setPreferredSize(new Dimension(160, 380));
+		mainPanel.setPreferredSize(new Dimension(260, 380));
 		mainPanel.add(text, BorderLayout.CENTER);
 		mainPanel.add(buttonPanel, BorderLayout.SOUTH);
 		
@@ -50,4 +69,5 @@ public class MainForm extends JFrame {
 		this.add(panel);
 		pack();
 	}
+
 }
