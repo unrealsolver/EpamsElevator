3ee0bdac2d3cc6ebf8f9826aa8c7860dee5d3d0d
Ruslan Panasiuk <solver@tut.by>
2013-03-28 17:38:34 +0300

    From eclipse

--- src/by/epamlab/elevator/ui/BaseWidget.java
+++ src/by/epamlab/elevator/ui/BaseWidget.java
@@ -1,12 +1,16 @@
 package by.epamlab.elevator.ui;
 
 import java.awt.Graphics;
+import java.awt.geom.Dimension2D;
 
-public class WBox implements DynamicWidget {
+public class BaseWidget implements DynamicWidget {
+	private Dimension2D position;
+	private Dimension2D size;
+	
 	@Override
 	public void draw(Graphics target) {
 		target.drawRect(15, 15, 80, 50);
-		
+		size.x;
 	}
 	@Override
 	public void update(float dt) {
