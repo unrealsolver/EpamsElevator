6884ffbe659486331945283f342f79bfe6ff4955
Ruslan Panasiuk <solver@tut.by>
2013-04-08 15:11:30 +0300

    Simple WStorey textured widget

---
+++ src/by/epamlab/elevator/ui/WStorey.java
@@ -0,0 +1,47 @@
+package by.epamlab.elevator.ui;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.jsfml.system.Vector2i;
+
+public class WStorey extends WBase {
+	private BufferedImage image;
+	private boolean safemode;
+	
+	public WStorey() {
+		super();
+		init();
+	}
+	
+	public WStorey(Vector2i position) {
+		super(position);
+		init();
+	}
+	
+	public void init() {
+		try {
+			image = ImageIO.read(new File("resources/storey.png"));
+			safemode = false;
+		} catch (IOException e) {
+			safemode = true;
+			System.err.println("Cant load image! Working in safe mode!");
+		}
+	}
+	
+	@Override
+	public void draw(Graphics target) {
+		super.draw(target);
+		if (!safemode) {
+			target.drawImage(image, position.x, position.y, null);
+		}
+	}
+	
+	@Override
+	public void update(float dt) {
+		//pass
+	}
+}
