9aac2a7c2a732078ffb7b56f1e5341fc5def7705
Ruslan Panasiuk <solver@tut.by>
2013-04-08 12:35:03 +0300

    Added missed passenger's transportation state

--- src/core/Passenger.java
+++ src/core/Passenger.java
@@ -3,10 +3,12 @@ package core;
 public class Passenger {
 	private final int id;
 	private final int destinationStorey;
+	private TransportationState state;
 
 	public Passenger(int destinationStorey) {
 		id = IDManager.getNewID();
 		this.destinationStorey = destinationStorey;
+		this.setState(TransportationState.NOT_STARTED);
 	}
 
 	public int getId() {
@@ -15,6 +17,14 @@ public class Passenger {
 
 	public int getDestinationStorey() {
 		return destinationStorey;
+	}
+
+	public TransportationState getState() {
+		return state;
+	}
+
+	public void setState(TransportationState state) {
+		this.state = state;
 	}
 
 }
